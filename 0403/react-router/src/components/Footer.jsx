3a5043faa28e7a0001009dff8a26b4561a658168
ljhmd00 <lmsmd00@naver.com>
2023-04-03 16:28:56 +0900

    Add header,footer

---
+++ 0403/react-router/src/components/Footer.jsx
@@ -0,0 +1,4 @@
+function Footer() {
+    return <footer className="bg-green-100 flex  justify-center items-center h-20">footer</footer>;
+}
+export default Footer;
---
+++ 0403/react-router/src/components/Header.jsx
@@ -0,0 +1,4 @@
+function Header() {
+    return <header className="bg-purple-100 flex  justify-center items-center h-20">header</header>;
+}
+export default Header;
