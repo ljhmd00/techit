f298665e8628a8c316f0a35ce680971eaa41e37f
ljhmd00 <lmsmd00@naver.com>
2023-04-03 16:34:26 +0900

    add routes

--- 0403/react-router/src/App.jsx
+++ 0403/react-router/src/App.jsx
@@ -1,5 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import A from "./pages/a";
+import B from "./pages/b";
+import C from "./pages/c";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Main />}></Route>
+                <Route path="/a" element={<A />}></Route>
+                <Route path="/b" element={<B />}></Route>
+                <Route path="/c" element={<C />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- 0403/react-router/src/components/Header.jsx
+++ 0403/react-router/src/components/Header.jsx
@@ -1,4 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-    return <header className="bg-purple-100 flex  justify-center items-center h-20">header</header>;
+    return (
+        <header className="bg-purple-100 flex  justify-center items-center h-20">
+            <Link to="/">
+                <button className="bg-red-300 btnStyle">메인</button>
+            </Link>
+            <Link to="/a">
+                <button className="bg-blue-300 btnStyle">A</button>
+            </Link>
+            <Link to="/b">
+                <button className="bg-green-300 btnStyle">B</button>
+            </Link>
+            <Link to="/c">
+                <button className="bg-yellow-300 btnStyle">C</button>
+            </Link>
+        </header>
+    );
 }
 export default Header;
