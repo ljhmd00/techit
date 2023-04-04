b50f76e08566cebf501f54dfee253438b03ebfa4
ljhmd00 <lmsmd00@naver.com>
2023-04-04 13:52:57 +0900

    useEffect, useState, props

--- 0404/effect/src/App.jsx
+++ 0404/effect/src/App.jsx
@@ -1,5 +1,26 @@
+import Counter from "./components/Counter";
+import { useState } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+    const [toggle, setToggle] = useState(false);
+    const [count, setCount] = useState(0);
+    const onClickToggle = () => {
+        setToggle(!toggle);
+    };
+
+    return (
+        <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+            {/* {toggle ? <Counter /> : "카운터"} */}
+            {toggle && <Counter count={count} setCount={setCount} />}
+            <button
+                onClick={onClickToggle}
+                className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded-full
+"
+            >
+                버튼
+            </button>
+        </div>
+    );
 }
 
 export default App;
---
+++ 0404/effect/src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function Counter({ count, setCount }) {
+    const onClickAdd = () => {
+        setCount(count + 1);
+    };
+
+    useEffect(() => {
+        console.log("Counter 컴포넌트가 생성된 상택");
+
+        if (count > 0) {
+            console.log("Counter 업데이트");
+        }
+        return () => {
+            console.log("Counter 제거");
+        };
+    }, [count]);
+
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className="text-center text-4xl">{count}</div>
+            <button onClick={onClickAdd} className="bg-red-500 px-4 py-2 rounded-lg mb-3 mt-3">
+                +
+            </button>
+        </div>
+    );
+}
+export default Counter;
