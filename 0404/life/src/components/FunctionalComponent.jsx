1c529191a62b01649c1843a9970945b5dbe90bf5
ljhmd00 <lmsmd00@naver.com>
2023-04-04 11:18:43 +0900

    add useEffect

--- 0404/life/src/components/FunctionalComponent.jsx
+++ 0404/life/src/components/FunctionalComponent.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 const FunctionalComponent = () => {
-    useEffect();
+    useEffect(() => {}, []);
 
     return <div className="bg-blue-100  h-96">FunctionComponent</div>;
 };
--- 0404/life/src/components/FunctionalComponents.jsx
+++
@@ -1,5 +0,0 @@
-const FunctionalComponent = () => {
-    return <div className="bg-blue-100">FunctionComponent</div>;
-};
-
-export default FunctionalComponent;
