c151db1482f4399c31a1e1fb0c938d50b8c938a3
ljhmd00 <lmsmd00@naver.com>
2023-04-06 16:45:29 +0900

    0406 worldcupGame

--- 0406/worldcup/src/components/AnimalCard.jsx
+++ 0406/worldcup/src/components/AnimalCard.jsx
@@ -1,7 +1,19 @@
-const AnimalCard = ({ animal }) => {
+const AnimalCard = ({ animal, choice, onClickChoice }) => {
     return (
-        <div>
-            <img src={`${process.env.PUBLIC_URL}/images/${animal}.jpeg`} />
+        <div className="flex flex-col justify-center items-center">
+            <img
+                className="border-8 border-white shadow-lg shadow-white w-96"
+                src={`${process.env.PUBLIC_URL}/images/${animal}.jpeg`}
+            />
+            <div className="text-2xl mt-4 font-bold">{animal}❤❤❤</div>
+            <button
+                className={`bg-gray-500 text-white text-2xl mt-4 px-4 py-2 rounded-md ${
+                    choice % 2 === 0 ? "bg-green-400" : "bg-red-400"
+                }`}
+                onClick={onClickChoice(animal)}
+            >
+                선택
+            </button>
         </div>
     );
 };
--- 0406/worldcup/src/pages/worldcup.jsx
+++ 0406/worldcup/src/pages/worldcup.jsx
@@ -1,24 +1,64 @@
+import { useEffect } from "react";
 import animalData from "../animalData.json";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import AnimalCard from "../components/AnimalCard";
+import WinAnimalCard from "../components/WinAnimalCard";
 
 const Worldcup = () => {
     const [shuffleAnimal, setShuffleAnimal] = useState();
+    const [choice, setChoice] = useState(0);
+    const [nextRound, setNextRound] = useState([]);
+    const [end, setEnd] = useState(16);
+
+    const onClickChoice = (v) => () => {
+        setChoice(choice + 2);
+        // let copy = [...nextRound];
+        // copy.push(v);
+        // setNextRound(copy);
+        setNextRound([...nextRound, v]);
+    };
 
     useEffect(() => {
         let shuffleAnimalData = animalData.sort(() => {
-            return -1;
+            return Math.random() - 0.5;
         });
-        console.log(shuffleAnimalData);
+        setShuffleAnimal(shuffleAnimalData);
     }, []);
 
-    // animalData 랜덤 정렬
-    // useEffect(() => {
-    //     let shuffleAnimalData = animalData.sort(() => {
-    //         return Math.random() - 0.5;
-    //     });
-    //     setShuffleAnimal(shuffleAnimalData);
-    // }, []);
+    useEffect(() => {
+        if (choice === end) {
+            setShuffleAnimal(nextRound);
+            setNextRound([]);
+            setEnd(end / 2);
+            setChoice(0);
+        }
+    }, [choice]);
 
-    return <div>worldcup</div>;
+    return (
+        <div className="bg-blue-100 min-h-screen flex justify-center items-center">
+            {shuffleAnimal &&
+                (end === 1 ? (
+                    <WinAnimalCard animal={shuffleAnimal[choice]} />
+                ) : (
+                    <>
+                        <AnimalCard
+                            animal={shuffleAnimal[choice]}
+                            choice={choice}
+                            onClickChoice={onClickChoice}
+                        />
+                        <div className="text-2xl mx-8 font-bold text-center">
+                            <div>{`${end === 2 ? "결승" : end + "강"}`}</div>
+                            <div>VS</div>
+                        </div>
+                        <AnimalCard
+                            animal={shuffleAnimal[choice + 1]}
+                            choice={choice + 1}
+                            onClickChoice={onClickChoice}
+                        />
+                    </>
+                ))}
+        </div>
+    );
 };
+
 export default Worldcup;
