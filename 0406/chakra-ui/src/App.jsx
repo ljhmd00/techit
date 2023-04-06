10bad1b51e593dad3cbea566f6892cc97e855e3f
ljhmd00 <lmsmd00@naver.com>
2023-04-06 10:12:18 +0900

    chakra_icons, Button

--- 0406/chakra-ui/src/App.jsx
+++ 0406/chakra-ui/src/App.jsx
@@ -1,10 +1,21 @@
 import { ChakraProvider, Box, Button } from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 function App() {
+    const [isLoading, setisLoading] = useState(true);
+
     return (
         <ChakraProvider>
             <Box>Hello, ChakraUI</Box>
-            <Button colorScheme="twitter">button</Button>
+            <Button
+                isLoading={isLoading}
+                colorScheme="twitter"
+                leftIcon={<PhoneIcon />}
+                variant="solid"
+            >
+                Call
+            </Button>
         </ChakraProvider>
     );
 }
